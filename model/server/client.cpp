77d1d95d3bb7244428f5781626dee2eae32d44ff
Jehaechan <71350045+Jehaechan@users.noreply.github.com>
2023-06-09 05:28:59 +0900

    commit client.cpp to remove

--- model/server/client.cpp
+++ model/server/client.cpp
@@ -1,7 +1,3 @@
-/*
-	 [프로젝트 속성]->[구성 속성]->[링커]->[입력]->[추가 종속성]에 ws2_32.lib를 추가
-	 [프로젝트 속성]->[구성 속성]->[매니페스트 도구]->[입력 및 출력] ->[매니페스트 포함]을 아니요로 변경
-*/
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
@@ -10,19 +6,26 @@
 #include "socket_client.h"
 
 using namespace std;
+char buffer[10000];
 
 int main(int argc, char* argv[]) {
 	connect_server();
 	printf("connected!\n");
 
-	int bufsize = recv_data_size();
-	cout << bufsize << endl;
-
-	char* recv_buf = (char*)malloc(sizeof(char) * (bufsize + 1));
-	recv_data(recv_buf, bufsize);
-
-	printf("%s", recv_buf);
-	free(recv_buf);
+	int size;
+	char datasize[20];
+	while (1) {
+		memset(datasize, 0, 20);
+		read_data(datasize, 20);
+		char ok[3] = "ok";
+		send_data(ok, 3);
+		size = atoi(datasize);
+		if (size == 0)
+			break;
+		read_data(buffer, size);
+		buffer[size + 1] = '\0';
+		printf("%s", buffer);
+	}
 
 	return 0;
 }
